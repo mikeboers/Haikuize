fdc2bc058772c51aa9b4231e2aba7c607c2c2f2d
Mike Boers <github@mikeboers.com>
2013-02-18 22:08:14 -0800

    Clean and slight refactor

--- haikuize.py
+++ haikuize.py
@@ -1,25 +1,33 @@
 import os
 import sys
 import re
-import curses
-from curses.ascii import isdigit
+from optparse import OptionParser
 
 import nltk.corpus
 
 cmudict = nltk.corpus.cmudict.dict() 
 
-def nsyl(word):
+
+def syllable_count(word):
+    """Estimate number of syllables in a given word.
+
+    Returns None if it cannot tell.
+
+    """
+
     key = cmudict.get(word.lower())
     if not key:
         return
     return len([1 for x in key[0] if x[-1].isdigit()])
 
-def corpus_iter():
-    for line in sys.stdin:
-        for word in line.strip().split():
-            yield word
 
-def find_nsyl(count, words, start=0):
+def isolate_syllable_count(count, words, start=0):
+    """Return end index to slice given words so they sum to the given count.
+
+    Returns False or an integer, where sum(syllable_count(x) for x in words[:end]) == count.
+
+    """
+
     total = 0
     for i in xrange(start, len(words)):
         word, n = words[i]
@@ -30,32 +38,71 @@ def find_nsyl(count, words, start=0):
             return False
     return False
 
-corpus = corpus_iter()
-    
-words = []
-while True:
-    
-    while len(words) < 17:
-        word = next(corpus)
-        word = re.sub(r'^\W+', '', word)
-        word = re.sub(r'\W+$', '', word)
-        n = nsyl(word)
-        if not n:
+
+def normalize_word(word):
+    word = re.sub(r'^\W+', '', word)
+    word = re.sub(r'\W+$', '', word)
+    return word
+
+
+if __name__ == '__main__':
+
+    option_parser = OptionParser(usage='%prog [options]')
+    option_parser.add_option('-s', '--match-start', action='store_true')
+    option_parser.add_option('-e', '--match-end', action='store_true')
+    opts, args = option_parser.parse_args()
+
+    if not args:
+        files = [sys.stdin]
+    else:
+        files = [open(x) for x in args]
+
+    def corpus_iter():
+        for fh in files:
+            for line in fh:
+                for word in line.strip().split():
+                    yield word
+
+                    if opts.match_end and '.' in word:
+                        yield None
+
+            yield None
+
+
+    corpus = corpus_iter()
+        
+
+    words = []
+    while True:
+        
+        while len(words) < 17:
+
+            word = next(corpus)
+            if not word:
+                words = []
+                continue
+
+            word = normalize_word(word)
+            n = syllable_count(word)
+            if not n:
+                words = []
+                continue
+            words.append((word, n))
+        
+        while opts.match_end and word:
+            word = next(corpus)
+
+        a = isolate_syllable_count(5, words, 0)
+        b = a and isolate_syllable_count(7, words, a)
+        c = b and isolate_syllable_count(5, words, b)
+        
+        if c:
+            print ' '.join(x[0] for x in words[0:a]).title()
+            print ' '.join(x[0] for x in words[a:b]).title()
+            print ' '.join(x[0] for x in words[b:c]).title()
+            print
+        
+        if opts.match_start:
             words = []
-            continue
-        words.append((word, n))
-    
-    a = find_nsyl(5, words, 0)
-    b = a and find_nsyl(7, words, a)
-    c = a and b and find_nsyl(5, words, b)
-    
-    if a and b and c:
-        print ' '.join(x[0] for x in words[0:a]).title()
-        print ' '.join(x[0] for x in words[a:b]).title()
-        print ' '.join(x[0] for x in words[b:c]).title()
-        print
-    
-    words.pop(0)
-
-
-
+        else:
+            words.pop(0)
